9d7bc16f4dd887f5d531dfeedd2e8b15ab042797
Matthew Blode <matthew@linktr.ee>
2025-12-31 07:19:51 +1100

    Ultracite and husky

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import glsl from 'vite-plugin-glsl'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import glsl from "vite-plugin-glsl";
 
 export default defineConfig({
   plugins: [
     react(),
     glsl({
-      include: [
-        '**/*.glsl',
-        '**/*.vert',
-        '**/*.frag',
-        '**/*.vs',
-        '**/*.fs'
-      ],
+      include: ["**/*.glsl", "**/*.vert", "**/*.frag", "**/*.vs", "**/*.fs"],
       exclude: undefined,
       warnDuplicatedImports: true,
-      defaultExtension: 'glsl',
+      defaultExtension: "glsl",
       compress: false,
-      root: '/'
-    })
+      root: "/",
+    }),
   ],
   server: {
     port: 3000,
-    open: true
-  }
-})
+    open: true,
+  },
+});
